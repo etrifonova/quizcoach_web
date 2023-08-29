17c198c8203bc7bd8a0bd45ca05cb6d9fa4c3282
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2023-08-29 12:34:29 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# [Irregular-Verbs-App Version 2.0](https://etrifonova.github.io/Irregular-Verbs-App-2.0/)
+
+
+Current plans for app upgrade:
+- expand the list of verbs (completed on March 7);
+- add past participles  (completed on March 7);
+- add automatic generation of infinitives in case of correct answers;
+- put the cursor in the input field automatically after the generate button in pushed;
+- mobile version: make infinitives lighter in color.
---
+++ script.js
@@ -0,0 +1,145 @@
+
+let questionDisplay = document.querySelector('.out-1');
+let scoreBlock = document.querySelector('.score-result');
+let questions;
+let randomElement;
+let randomQuestion;
+
+const PART_ONE = [
+  {question: 'Для Пушкина ИМ был Данзас. А кто был ИМ для Гарри в первой книге?', answer: 'Рон Уизли'},
+  {question: 'Сначала был “капут драконис”, а потом?', answer: 'поросячий пятачок'},
+  {question: 'Несмотря на свое латинское название Mucus ad Nauseam, ЭТО ЗАКЛИНАНИЕ было переведено на русский не самым очевидным образом. Одно из слов отсылает к тому, что было у каждого факультета. Назовите ЭТО ЗАКЛИНАНИЕ двумя словами, начинающимися на одну и ту же букву.', answer: 'проклятие призраков'},
+  {question: 'Данный персонаж упоминается в первой и последних книгах. Про него известно, что он в эпоху раннего средневековья держал в страхе всю Южную Англию.', answer: 'Эмерик Отъявленный'},
+  {question: 'Олух! Пузырь! Остаток! Уловка! - кому принадлежат эти слова?', answer: 'Альбус Дамблдор'},
+  {question: 'О ком это: “Тощее пыльно-серое создание с выпученными горящими глазами”?', answer: 'Миссис Норрис'},
+  {question: 'По мнению близнецов, один из преподавателей носил под одеждой ЕГО в целях защиты. Назовите ЕГО.', answer: 'чеснок'},
+  {question: 'Какой факультет набрал 352 балла?', answer: 'Пуффендуй'},
+  {question: 'Для избавления от чего предназначено зелье со следующим составом: змеиные зубы, рогатый слизень, иглы дикобраза?', answer: 'прыщи'},
+  {question: 'Некое животное является патронусом для волшебника, близкому Гарри, хоть и не одной крови. Также однажду Гарри отверг бутерброд с его мясом. Назовите это животное.', answer: 'горностай'},
+  {question: 'Какой персонаж загадан в этом описании? "Крошечный человечек в высоком фиолетовом цилиндре. По словам профессора Макгонагалл, не отличается особым умом. Но Альбус Дамблдор считает его весьма полезным."', answer: 'Дедалус Дингл'},
+]
+
+const PART_TWO = [
+  {question: 'Сколько маглов погубило проклятое ожерелье из "Горбин и Бэркес"?', answer: '19'},
+  {question: 'Как называется процедура избавления от гномов?', answer: 'дегномизация'},
+  {question: 'Сколько машин Ford Anglia было разбито на съемках "Тайной комнаты"?', answer: '14'}
+]
+
+const PART_THREE = [
+  {question: 'Как называется магический прибор, используемый волшебниками для предупреждения об опасности?', answer: 'вредноскоп'},
+  {question: 'Набор по уходу за чем подарила Гермиона Гарри на день рождения?', answer: 'метла'},
+  {question: 'Какой учебник читает Гарри в начале "Узника Азкабана"?', answer: 'история магии'},
+  {question: 'Сколько раз была сожжена на костре Венделина Странная?', answer: '47'},
+  {question: 'Сколько галлеонов выиграл Артур Уизли от "Ежедневного пророка"?', answer: '700'},
+]
+
+const PART_FOUR = [
+  {question: 'В каком городке находится особняк Рэддлов?', answer: 'Литтл Хэнглтон'},
+  {question: 'Как называется магловский трактир в Литтл Хэнглтоне?', answer: 'висельник'},
+  {question: 'Как зовут садовника Рэддлов?', answer: 'Фрэнк Брайс'},
+]
+
+const PART_FIVE = [
+  {question: 'Нимфадора Тонкс воспользовалась ИМ  в доме Дурслей, чтобы убрать из клетки Букли помет и перья. ИМ же воспользовалась Джинни Уизли, чтобы убрать Смердящий сок с лица и груди Гарри Поттера. Догадавшись, о чем речь, напишите слово для его вызова.', answer: 'Экскуро'},
+  {question: 'Как называется заклинание для передвижения предметов по воздуху?', answer: 'локомотор'},
+  {question: 'Какое заклятие Миссис Уизли наложила на кухонную дверь?', answer: 'заклятие недосягаемости'},
+]
+
+const PART_SIX = [
+  {question: 'Некоему персонажу из первой части книги пришлось значительно больше времени проводить дома по этой причине. Назовите эту причину двумя словами.', answer: 'заклятие империус'},
+  {question: 'ОН назван в честь некоего города в английском графстве Девоне, на левой стороне маленькой реки Аксе. Также известно, что ОН пострадал от некоторых действий Корнелиуса Фаджа. Назовите ЕГО.', answer: 'Аксминстерский ковер'},
+  {question: 'Кто так описан: «В густой гриве рыжевато-каштановых волос и в кустистых бровях виднелись седые пряди, из-за очков в проволочной оправе смотрели пронзительные желтые глаза, а в движениях, хоть он и прихрамывал, сквозила своеобразная гибкая, размашистая грация.»', answer: 'Руфус Скримджер'},
+]
+
+const PART_SEVEN = [
+  {question: 'Какую метлу получил Рон за то, что стал старостой?', answer: 'Чистомет-11'},
+  {question: '«музыкальная шкатулка, ___________, несколько старинных печатей; и, наконец, орден Мерлина первой степени». Что пропущено в этом списке?', answer: 'медальон'},
+  {question: '«Похоже, что ОНО читает мысли или, по крайней мере, настроение своей владелицы, поскольку владелица ЕМУ ничего не диктует.» Назовите ЕГО тремя словами.', answer: 'прытко пишущее перо'},
+]
+
+
+function getddl() {
+  let x = formid.ddlselect[formid.ddlselect.selectedIndex].value;
+  document.getElementById('lblmess').innerHTML=("Выбранная категория: " + formid.ddlselect[formid.ddlselect.selectedIndex].text);
+  console.log(x);
+  switch(x) {
+    case 'part1':
+      questions = PART_ONE;
+      break;
+  
+    case 'part2':
+      questions = PART_TWO;
+      break;
+  
+    case 'part3':
+      questions = PART_THREE;
+      break;
+  
+    case 'part4':
+      questions = PART_FOUR;
+      break;
+  
+    case 'part5':
+      questions = PART_FIVE;
+      break;
+  
+    case 'part6':
+      questions = PART_SIX;
+      break;
+  
+    case 'part7':
+      questions = PART_SEVEN;
+      break;
+  
+    default:
+      questions = PART_ONE;
+  }
+}
+
+function genQuestion() {
+
+  if (questions.length === 0) {
+    questionDisplay.innerHTML = 'Вопросы закончились'
+  } else {
+    randomElement = questions.map(element => element)[Math.floor(Math.random()*questions.length)];
+    randomQuestion = randomElement.question;
+    questionDisplay.innerHTML = randomQuestion;
+    message.innerHTML = '';
+    userInput.value = '';
+  }
+  }
+
+  let userInput = document.querySelector('.user-answer-input');
+  let message = document.querySelector('.out-2');
+  let score = 0;
+
+  function checkForms() {
+    
+    let correctAnswer = questions.find((element) => {
+      return element.question == randomQuestion
+    }).answer.trim().toLowerCase();
+    
+    if (userInput.value.trim().toLowerCase() === correctAnswer) {
+        message.innerHTML = 'Верно!';
+        score++;
+        scoreBlock.innerHTML = score;
+        console.log(questions.indexOf(randomElement))
+        questions.splice(questions.indexOf(randomElement), 1);
+    } else {
+      console.log(userInput.value)
+        message.innerHTML = 'Неверно!'
+    }
+}
+
+// solution found here https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
+
+
+userInput.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    checkForms();
+  }
+});
